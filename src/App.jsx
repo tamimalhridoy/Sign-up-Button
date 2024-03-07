ecbc9e2b136eea328106afed350f4668ff5fae2f
tamimalhridoy <tamimalhridoy3@gmail.com>
2024-03-07 15:42:12 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+function App() {
+  return (
+    <>
+      <section className=" flex justify-center mt-72">
+      <button>
+        Sign up
+        <div class="arrow-wrapper">
+          <div class="arrow"></div>
+        </div>
+      </button>
+      </section>
+    </>
+  );
+}
+
+export default App;
